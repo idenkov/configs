85296f7b710a80a8230b6c966cc3172cc2d0ef71
Ivan Denkov <ivan.denkov87@gmail.com>
2018-06-09 03:26:28 +0300

    laptop brightness

---
+++ brightness_up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CAT="$(which cat)"
+current_brightness=`$CAT /sys/class/backlight/intel_backlight/brightness`
+new_brightness=$(($current_brightness+20))
+echo $new_brightness > /sys/class/backlight/intel_backlight/brightness
